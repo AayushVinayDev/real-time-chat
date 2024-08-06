72a8393347763afa2f355b5cd77cc3a4b26dab5b
Aayush Vinay <btech10104.19@bitmesra.ac.in>
2024-08-06 10:31:03 +0530

    backend complete

---
+++ server/internal/ws/ws_handler.go
@@ -0,0 +1,115 @@
+package ws
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type Handler struct {
+	hub *Hub
+}
+
+func NewHandler(hub *Hub) *Handler {
+	return &Handler{hub: hub}
+}
+
+type CreateRoomReq struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h *Handler) CreateRoom(c *gin.Context) {
+	var req CreateRoomReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	h.hub.Rooms[req.ID] = &Room{
+		ID:      req.ID,
+		Name:    req.Name,
+		Clients: make(map[string]*Client),
+	}
+
+	c.JSON(http.StatusOK, req)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true // TODO: Add origin verification
+	},
+}
+
+func (h *Handler) JoinRoom(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	roomID := c.Param("roomID")
+	clientID := c.Query("userID")
+	username := c.Query("username")
+
+	cl := &Client{
+		Conn:     conn,
+		Message:  make(chan *Message, 10),
+		ID:       clientID,
+		RoomID:   roomID,
+		Username: username,
+	}
+
+	m := &Message{
+		Content:  "A new user has joined the room",
+		RoomID:   roomID,
+		Username: username,
+	}
+
+	h.hub.Register <- cl
+
+	h.hub.Broadcast <- m
+
+	go cl.writeMessage()
+	cl.readMessage(h.hub)
+
+}
+
+type RoomRes struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h *Handler) GetRooms(c *gin.Context) {
+	rooms := make([]RoomRes, 0)
+
+	for _, room := range h.hub.Rooms {
+		rooms = append(rooms, RoomRes{ID: room.ID, Name: room.Name})
+	}
+
+	c.JSON(http.StatusOK, rooms)
+}
+
+type ClientRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
+
+func (h *Handler) GetClients(c *gin.Context) {
+	roomID := c.Param("roomID")
+	var clients []ClientRes
+
+	if _, ok := h.hub.Rooms[roomID]; !ok {
+		clients := make([]ClientRes, 0)
+		c.JSON(http.StatusOK, clients)
+	}
+
+	for _, client := range h.hub.Rooms[roomID].Clients {
+		clients = append(clients, ClientRes{ID: client.ID, Username: client.Username})
+	}
+
+	c.JSON(http.StatusOK, clients)
+}
